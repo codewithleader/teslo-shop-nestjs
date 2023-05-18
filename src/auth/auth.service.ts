f7bfc25b8d2cdf256aae933e0fa6858c6656db6a
ElisPerez <elisperezmusic@gmail.com>
2023-05-17 21:30:06 -0500

    login user part1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,12 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 //
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 // bcrypt
 import * as bcrypt from 'bcrypt';
@@ -18,7 +19,7 @@ export class AuthService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async createUser(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
     try {
       const { password, ...userData } = createUserDto;
       const user = this.userRepository.create({
@@ -37,6 +38,24 @@ export class AuthService {
     } catch (error) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    // En el User Entity el password configuramos para que por defecto no se muestre en las consultas (find, findOne, findOneBy), pero aquí sí necesitamos el password para hacer la verificación del password. Con el "select" indicamos cuales son los campos que nos interesa que regrese la consulta y ahí especificamos el password.
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if (!user) throw new UnauthorizedException('Credentials not valid (email)');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials not valid (password)');
+
+    // todo: retornar JWT
+    return user;
   }
 
   private handleDBErrors(error: any): never {
