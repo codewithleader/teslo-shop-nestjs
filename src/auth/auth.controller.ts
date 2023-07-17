5de0e1203840ee5f18cb2ca760de848bca0e83bd
ElisPerez <elisperezmusic@gmail.com>
2023-07-16 23:28:36 -0500

    verificar roles de usuario parte 1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   UseGuards,
   Headers,
+  SetMetadata,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 //
@@ -13,6 +14,7 @@ import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthService } from './auth.service';
 import { GetUser, RawHeaders } from './decorators';
 import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -55,7 +57,8 @@ export class AuthController {
 
   // Decoradores
   @Get('private2')
-  @UseGuards(AuthGuard())
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
   // Nombre de la función a continuación:
   privateRoute2(@GetUser() user: User) {
     return {
---
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -0,0 +1,24 @@
+import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+  constructor(
+    //
+    private readonly reflector: Reflector,
+  ) {}
+
+  // Para que un Guard sea válido tiene que implementar el metodo "canActivate()"
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[] = this.reflector.get(
+      'roles',
+      context.getHandler(),
+    );
+
+    console.log({ validRoles });
+    return true;
+  }
+}
